e31244e5547c5a5f6f50be52c2a85c760de07a26
devinschulist <90712490+devinschulist@users.noreply.github.com>
2021-10-07 13:46:10 -0600

    Update DisPanelTest.java

--- imagelab/DisPanelTest.java
+++ imagelab/DisPanelTest.java
@@ -1,1 +1,29 @@
 package imagelab;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class DispPanelTest {
+    /**
+     * Sets up the test fixture.
+     * (Called before every test case method.)
+     */
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * Tears down the test fixture.
+     * (Called after every test case method.)
+     */
+    @After
+    public void tearDown() {
+    }
+
+
+
+
+}
